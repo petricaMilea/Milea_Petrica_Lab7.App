c91105526e9333eec738b2f0529c46cd1bf9a6ee
petricaMilea <petrica.v.milea@gmail.com>
2022-12-10 11:58:31 +0200

    10. Adaugam o  clasa in folderul Data astfel: click-dreapta pe numele folderului Add->Class si ii dam numele ShoppingListDatabase. Aceasta clasa va contine cod pentru crearea, citirea, scrierea si stergerea datelor

---
+++ Data/ShoppingListDatabase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using Milea_Petrica_Lab7.Models;
+
+namespace Milea_Petrica_Lab7.Data
+{
+    public class ShoppingListDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+        public ShoppingListDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<ShopList>().Wait();
+        }
+
+        public Task<List<ShopList>> GetShopListsAsync()
+        {
+            return _database.Table<ShopList>().ToListAsync();
+        }
+
+        public Task<ShopList> GetShopListAsync(int id)
+        {
+            return _database.Table<ShopList>()
+                            .Where(i => i.ID == id)
+                            .FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveShopListAsync(ShopList slist)
+        {
+            if (slist.ID != 0)
+            {
+                return _database.UpdateAsync(slist);
+            }
+            else
+            {
+                return _database.UpdateAsync(slist);
+            }
+        }
+
+        public Task<int> DeleteShopListAsync(ShopList slist)
+        {
+            return _database.DeleteAsync(slist);
+        }
+    }
+}
