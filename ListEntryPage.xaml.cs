d96d5d677773c744a6503ecd3e8b8fec0e3fe361
petricaMilea <petrica.v.milea@gmail.com>
2022-12-10 12:25:41 +0200

    ex 14

--- ListEntryPage.xaml.cs
+++ ListEntryPage.xaml.cs
@@ -1,3 +1,5 @@
+using Milea_Petrica_Lab7.Models;
+
 namespace Milea_Petrica_Lab7;
 
 public partial class ListEntryPage : ContentPage
@@ -6,4 +8,27 @@ public partial class ListEntryPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		listView.ItemsSource = await App.Database.GetShopListsAsync();
+	}
+
+	async void OnShopListAddedClicked(object sender, EventArgs e)
+	{
+		await Navigation.PushAsync(new ListEntryPage
+		{
+			BindingContext = new ShopList()
+		});
+	}
+
+	async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem != null) 
+		{ 
+			await Navigation.PushAsync(new ListEntryPage 
+			{ 
+				BindingContext = e.SelectedItem as ShopList }); }
+    }
 }
