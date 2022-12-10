cf720af345d79706c1c1fc8b9374ee75ed46d88a
petricaMilea <petrica.v.milea@gmail.com>
2022-12-10 12:37:31 +0200

    17. Din fereastra Solution Explorer deschidem fisierul ListPage.xaml.cs si adaugam la continutul acestuia codul de mai jos. Cand este apasat butonul Save, handler-ul de eveniment OnSaveButtonClicked este executat, lista de cumparaturi este salvata in baza de date si aplicatia navigheaza la pagina precedenta. La executarea handlerului de eveniment OnDeleteButtonClicked , lista de cumparaturi este stearsa din baza de date si aplicatia navigheaza la pagina precedenta

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -1,3 +1,5 @@
+using Milea_Petrica_Lab7.Models;
+
 namespace Milea_Petrica_Lab7;
 
 public partial class ListPage : ContentPage
@@ -5,5 +7,19 @@ public partial class ListPage : ContentPage
 	public ListPage()
 	{
 		InitializeComponent();
+	}
+
+    async void OnSaveButtonClicked(object sender, EventArgs e) 
+	{
+		var slist = (ShopList)BindingContext; 
+		slist.Date = DateTime.UtcNow; 
+		await App.Database.SaveShopListAsync(slist); 
+		await Navigation.PopAsync(); 
+	}
+    async void OnDeleteButtonClicked(object sender, EventArgs e) 
+	{
+		var slist = (ShopList)BindingContext; 
+		await App.Database.DeleteShopListAsync(slist); 
+		await Navigation.PopAsync(); 
 	}
 }
