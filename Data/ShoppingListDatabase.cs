e6eb650ce70237b85fe16be0f481a3122adea62e
petricaMilea <petrica.v.milea@gmail.com>
2022-12-12 22:12:24 +0200

    8. In fisierul Data/ShoppingListDatabase adaugam cod pentru crearea, citirea, editarea si stergerea datelor pentru clasa Product. Adaugam in Clasa ShoppingListDatabase

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -15,6 +15,8 @@ namespace Milea_Petrica_Lab7.Data
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
         }
 
         public Task<List<ShopList>> GetShopListsAsync()
@@ -44,6 +46,28 @@ namespace Milea_Petrica_Lab7.Data
         public Task<int> DeleteShopListAsync(ShopList slist)
         {
             return _database.DeleteAsync(slist);
+        }
+
+        public Task<int> SaveProductAsync(Product product)
+        {
+            if (product.ID != 0)
+            {
+                return _database.UpdateAsync(product);
+            }
+            else
+            {
+                return _database.InsertAsync(product);
+            }
+        }
+
+        public Task<int> DeleteProductAsync(Product product)
+        {
+            return _database.DeleteAsync(product);
+        }
+
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
         }
     }
 }
