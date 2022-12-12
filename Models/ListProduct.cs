273038630757bc666395f6c6e66504f5907576b2
petricaMilea <petrica.v.milea@gmail.com>
2022-12-12 21:42:53 +0200

    5. Adaugam o  clasa in folderul: Models astfel: click-dreapta pe numele folderului Add->Class si ii dam numele ListProduct

---
+++ Models/ListProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Milea_Petrica_Lab7.Models
+{
+    public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+
+        public int ProductID { get; set; }
+    }
+}
