67e085fde9c780ebf11b2e244bed50ea84cb36b7
petricaMilea <petrica.v.milea@gmail.com>
2022-12-15 21:04:36 +0200

    9. In fisierul ProductPage.xmls.cs adaugam

--- ProductPage.xaml.cs
+++ ProductPage.xaml.cs
@@ -1,3 +1,5 @@
+using Milea_Petrica_Lab7.Models;
+
 namespace Milea_Petrica_Lab7;
 
 public partial class ProductPage : ContentPage
@@ -5,5 +7,25 @@ public partial class ProductPage : ContentPage
 	public ProductPage()
 	{
 		InitializeComponent();
+	}
+
+	async void OnSaveButtonClicked(object sender, EventArgs e)
+	{
+		var product = (Product)BindingContext;
+		await App.Database.SaveProductAsync(product);
+		listView.ItemsSource = await App.Database.GetProductsAsync();
+	}
+
+	async void OnDeleteButtonClicked(object sender, EventArgs e)
+	{
+		var product = (Product)BindingContext;
+		await App.Database.DeleteProductAsync(product);
+		listView.ItemsSource = await App.Database.GetProductsAsync();
+	}
+
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		listView.ItemsSource = await App.Database.GetProductsAsync();
 	}
 }
