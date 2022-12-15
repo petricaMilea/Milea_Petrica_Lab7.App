770135d4bef327677d296312a0f1a2620479750e
petricaMilea <petrica.v.milea@gmail.com>
2022-12-15 21:22:21 +0200

    13. Dorim ca dupa selectarea unui produs aceasta sa se adauge la lista de cumparaturi si sa se intoarca la pagina ListPage. Adaugam in fisierul ProductPage.xaml.cs urmatorul cod:

--- ProductPage.xaml.cs
+++ ProductPage.xaml.cs
@@ -30,4 +30,19 @@ public partial class ProductPage : ContentPage
 		base.OnAppearing();
 		listView.ItemsSource = await App.Database.GetProductsAsync();
 	}
-}
+
+    async void OnAddButtonClicked(object sender, EventArgs e)
+    {
+        Product p;
+        if (listView.SelectedItem != null) 
+		{ 
+			p = listView.SelectedItem as Product; 
+			var lp = new ListProduct() 
+			{ 
+				ShopListID = sl.ID, 
+				ProductID = p.ID 
+			}; 
+			await App.Database.SaveListProductAsync(lp); 
+			p.ListProducts = new List<ListProduct> { lp }; 
+			await Navigation.PopAsync(); }
+    }
