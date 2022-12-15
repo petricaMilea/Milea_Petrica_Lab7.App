0e02a54e7fe8189e137257215460379ba4a7d033
petricaMilea <petrica.v.milea@gmail.com>
2022-12-15 21:17:12 +0200

    12. In fisierul ListPage.xaml.cs adaugam un urmatorul cod care va naviga la pagina ProductPage:

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -4,22 +4,29 @@ namespace Milea_Petrica_Lab7;
 
 public partial class ListPage : ContentPage
 {
-	public ListPage()
-	{
-		InitializeComponent();
-	}
+    public ListPage()
+    {
+        InitializeComponent();
+    }
 
-    async void OnSaveButtonClicked(object sender, EventArgs e) 
-	{
-		var slist = (ShopList)BindingContext; 
-		slist.Date = DateTime.UtcNow; 
-		await App.Database.SaveShopListAsync(slist); 
-		await Navigation.PopAsync(); 
-	}
-    async void OnDeleteButtonClicked(object sender, EventArgs e) 
-	{
-		var slist = (ShopList)BindingContext; 
-		await App.Database.DeleteShopListAsync(slist); 
-		await Navigation.PopAsync(); 
-	}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+
+    async void OnChooseButtonClicked(object sender, EventArgs e) 
+    { 
+        await Navigation.PushAsync(new ProductPage((ShopList)
+            this.BindingContext) 
+        { BindingContext = new Product() }); 
+    }
 }
