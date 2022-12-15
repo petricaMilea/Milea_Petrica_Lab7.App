6ad32ca57dcbcd95a369cf74948acd43052736ac
petricaMilea <petrica.v.milea@gmail.com>
2022-12-15 22:24:28 +0200

    modificari pentru corectare erori

--- ProductPage.xaml.cs
+++ ProductPage.xaml.cs
@@ -31,18 +31,23 @@ public partial class ProductPage : ContentPage
 		listView.ItemsSource = await App.Database.GetProductsAsync();
 	}
 
-    async void OnAddButtonClicked(object sender, EventArgs e)
-    {
-        Product p;
-        if (listView.SelectedItem != null) 
-		{ 
-			p = listView.SelectedItem as Product; 
-			var lp = new ListProduct() 
-			{ 
-				ShopListID = sl.ID, 
-				ProductID = p.ID 
-			}; 
-			await App.Database.SaveListProductAsync(lp); 
-			p.ListProducts = new List<ListProduct> { lp }; 
-			await Navigation.PopAsync(); }
-    }
+	async void OnAddButtonClicked(object sender, EventArgs e)
+	{
+		Product p;
+		if (listView.SelectedItem != null)
+		{
+			p = listView.SelectedItem as Product;
+			var lp = new ListProduct()
+			{
+				ShopListID = sl.ID,
+				ProductID = p.ID
+			};
+
+			await App.Database.SaveListProductAsync(lp);
+
+			p.ListProducts = new List<Product> { p };
+			await Navigation.PopAsync();
+		}
+		
+	}
+}
