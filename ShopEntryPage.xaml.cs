b607cf8ffc6c12cda7eb4e3ba839575dbc186897
petricaMilea <petrica.v.milea@gmail.com>
2023-01-10 21:19:56 +0200

    7. Din fereastra Solution Explorer deschidem fisierul ShopEntryPage.xaml.cs si adaugam la continutul acestuia codul de mai jos:

--- ShopEntryPage.xaml.cs
+++ ShopEntryPage.xaml.cs
@@ -1,3 +1,4 @@
+using Milea_Petrica_Lab7.Models;
 namespace Milea_Petrica_Lab7;
 
 public partial class ShopEntryPage : ContentPage
@@ -6,4 +7,29 @@ public partial class ShopEntryPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        listView.ItemsSource = await App.Database.GetShopsAsync();
+    }
+
+    async void OnShopAddedClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ShopPage
+        {
+            BindingContext = new Shop()
+        });
+    }
+
+    async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem != null) 
+        { 
+            await Navigation.PushAsync(new ShopPage 
+            { 
+                BindingContext = e.SelectedItem as Shop 
+            }); 
+        }
+    }
 }
