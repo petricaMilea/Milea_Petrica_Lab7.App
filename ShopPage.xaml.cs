e3359b4bce7c6feda227f0fda04096ed8b95ee50
petricaMilea <petrica.v.milea@gmail.com>
2023-01-10 21:49:12 +0200

    7.3. Din fereastra Solution Explorer deschidem fisierul ShopPage.xaml.cs si adaugam la continutul acestuia codul de mai jos. Cand este apasat butonul Save, handler-ul de eveniment OnSaveButtonClicked este executat, lista de cumparaturi este salvata in baza de date si aplicatia navigheaza la pagina precedenta.

--- ShopPage.xaml.cs
+++ ShopPage.xaml.cs
@@ -1,3 +1,5 @@
+using Milea_Petrica_Lab7.Models;
+
 namespace Milea_Petrica_Lab7;
 
 public partial class ShopPage : ContentPage
@@ -5,5 +7,12 @@ public partial class ShopPage : ContentPage
 	public ShopPage()
 	{
 		InitializeComponent();
+	}
+
+    async void OnSaveButtonClicked(object sender, EventArgs e) 
+	{ 
+		var shop = (Shop)BindingContext; 
+		await App.Database.SaveShopAsync(shop); 
+		await Navigation.PopAsync(); 
 	}
 }
