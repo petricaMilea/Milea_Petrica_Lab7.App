d00e71fa3e302c2953b3a2f6e5e97d9be27f6a42
petricaMilea <petrica.v.milea@gmail.com>
2023-01-10 23:36:09 +0200

    10. La apasarea butonului Show On Map se va declansa evenimentul Click care va fi gestionat de eventHandler-ul OnShowMapButtonClicked. Pentru a detecta locatia curenta a dispozitivului utilizam clasa Geolocation. Alternativ putem seta noi o locatie utilizand valori pentru latitudine si longitudine. In fisierul ShopPage.xaml.cs adaugam:

--- ShopPage.xaml.cs
+++ ShopPage.xaml.cs
@@ -15,4 +15,18 @@ public partial class ShopPage : ContentPage
 		await App.Database.SaveShopAsync(shop); 
 		await Navigation.PopAsync(); 
 	}
+
+    async void OnShowMapButtonClicked(object sender, EventArgs e)
+    {
+        var shop = (Shop)BindingContext; 
+        var address = shop.Adress; 
+        var locations = await Geocoding.GetLocationsAsync(address);
+
+        var options = new MapLaunchOptions { Name = "Magazinul meu preferat" };
+        var location = locations?.FirstOrDefault();
+
+        // var myLocation = await Geolocation.GetLocationAsync();
+        // var myLocation = new Location(46.7731796289, 23.6213886738);
+        await Map.OpenAsync(location, options);
+    }
 }
